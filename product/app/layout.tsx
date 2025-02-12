32f7a4eda68045aab4b1d851e3147ac7c2325eea
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-02-12 20:02:27 +0900

    [Chore] 프로젝트 환경 설정 (#2)
    * chore[#1] : NextJS with tailwind 프로젝트 생성
    
    * chore[#1] : install storybook
    
    * chore[#1] : 테일윈드 config 수정
    
    - src 하위 폴더도 인식 가능하도록 수정
    
    * chore[#1] : NextJS 기본 셋팅 제거
    
    * chore[#1] : tailwind , fsd prettier 추가
    
    * chore[#1] : 기본적인 NextJS eslint 설정
    
    * chore[#1]  : fsd  , tailwind css 프리티어 설치

---
+++ product/app/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./globals.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="kr">
+      <body>{children}</body>
+    </html>
+  );
+}
