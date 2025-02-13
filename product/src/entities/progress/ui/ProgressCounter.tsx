a73c1ab60d05291ca99c9b55b72b2ae43485e385
yonghyeun <ttddcc119@naver.com>
2025-02-13 11:35:40 +0900

    refactor[#3] : padLeftZero shared 에 테스트 코드와 함께 작성

--- product/src/entities/progress/ui/ProgressCounter.tsx
+++ product/src/entities/progress/ui/ProgressCounter.tsx
@@ -1,8 +1,7 @@
 import React from "react";
+import { padLeftZero } from "@/shared/lib/string";
 import { Text } from "@/shared/ui";
 import type { ProgressProps } from "./types";
-
-const padLeftZero = (num: number) => num.toString().padStart(2, "0");
 
 export const ProgressCounter: React.FC<ProgressProps> = ({
   current,
@@ -11,13 +10,13 @@ export const ProgressCounter: React.FC<ProgressProps> = ({
   return (
     <Text as="p" className="flex gap-1 font-semibold">
       <Text as="span" color="accent">
-        {padLeftZero(current)}
+        {padLeftZero(current, 2)}
       </Text>
       <Text as="span" color="accent">
         /
       </Text>
       <Text as="span" color="accent">
-        {padLeftZero(total)}
+        {padLeftZero(total, 2)}
       </Text>
     </Text>
   );
