143cbc41e01846d8654a9258469b99d5b90167f9
yonghyeun <ttddcc119@naver.com>
2025-02-13 14:03:36 +0900

    chore[#3] : 타입에 대한 un-used-vars lint 수정
    - 기존 unused-vars 를 제거하고 타입스크립트의 unused-vars 를 이용

--- product/eslint.config.mjs
+++ product/eslint.config.mjs
@@ -1,16 +1,26 @@
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['eslint:recommended', 'next', 'plugin:@next/next/recommended'],
+    extends: ["eslint:recommended", "next", "plugin:@next/next/recommended"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
   }),
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
